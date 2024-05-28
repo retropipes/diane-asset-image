6d078cd224dc59181a96446af900d12ad4c2ac59
RetroPipes <eric.ahnell@puttysoftware.com>
2024-05-28 13:10:25 -0400

    Initial commit.

---
+++ asset.image/src/main/java/org/retropipes/diane/asset/image/DianeImageIndex.java
@@ -0,0 +1,9 @@
+/*  Diane Game Engine
+Copyleft (C) 2019-present Eric Ahnell
+Any questions should be directed to the author via email at: support@puttysoftware.com */
+package org.retropipes.diane.asset.image;
+
+import org.retropipes.diane.asset.DianeAssetIndex;
+
+public interface DianeImageIndex extends DianeAssetIndex {
+}
